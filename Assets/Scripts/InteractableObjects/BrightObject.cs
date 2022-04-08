4f602fb7b147e1059abfda8f1a96f60f619a3ac3
Ilya <ganychev92@gmail.com>
2022-04-08 13:34:00 +0300

    Color bug edited

--- Assets/Scripts/InteractableObjects/BrightObject.cs
+++ Assets/Scripts/InteractableObjects/BrightObject.cs
@@ -8,15 +8,16 @@ public class BrightObject : BaseObject
     private Color _baseObjectColor;
     private void Start()
     {
-        _showObjectColor = new Color(0.05202916f, 0.6003655f, 0.745283f);
-        _baseObjectColor = GetComponent<Renderer>().material.color;
+        // _showObjectColor = new Color(0.05202916f, 0.6003655f, 0.745283f);
+         _baseObjectColor = GetComponent<Renderer>().material.color;
+        _showObjectColor = _baseObjectColor * 1.5f;
     }
     public override void StartAction()
     {
-        GetComponent<Renderer>().material.color  *= 1.5f;
+        GetComponent<Renderer>().material.color = _showObjectColor;
     }
     public override void RevertAction()
     {
-        GetComponent<Renderer>().material.color /= 1.5f;
+        GetComponent<Renderer>().material.color = _baseObjectColor;
     }
 }
--- Assets/Scripts/ScenarioSteps/ScenarioStep.cs
+++ Assets/Scripts/ScenarioSteps/ScenarioStep.cs
@@ -5,11 +5,11 @@ using UnityEngine.Events;
 
 public abstract class ScenarioStep : MonoBehaviour
 {
-    [SerializeField] protected PlayerCanvasController playerCanvasController;
-    [SerializeField] protected SoundPlayer soundPlayer;
-
     public UnityAction EndScenarioStepEvent;
     public UnityAction EndActionEvent;
+
+    [SerializeField] protected PlayerCanvasController playerCanvasController;
+    [SerializeField] protected SoundPlayer soundPlayer;
 
     protected TextHolder textHolder = new TextHolder();
     protected int action;
