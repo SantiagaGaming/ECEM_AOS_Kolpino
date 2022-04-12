c332ecf49a9b45e5396391d39299f50f307e7598
Ilya <ganychev92@gmail.com>
2022-04-12 13:56:51 +0300

    Added keys to Doors

--- Assets/Scripts/InteractableObjects/DoorObject.cs
+++ Assets/Scripts/InteractableObjects/DoorObject.cs
@@ -6,6 +6,8 @@ using UnityEngine.Events;
 public class DoorObject : InteractObject
 {
     public UnityAction DoorOpenedEvent;
+
+    [SerializeField] private GameObject _key;
     public override void StartObjectAction()
     {
         StartCoroutine(RotateDoor(true));
@@ -17,6 +19,24 @@ public class DoorObject : InteractObject
         {
             ActivateInteraction(false);
             DoorOpenedEvent?.Invoke();
+            _key.SetActive(true);
+         
+                _key.SetActive(true);
+                int keyX = 0;
+                while (keyX < 40)
+                {
+                    _key.transform.localPosition -= new Vector3(0, 0, 0.00001f);
+                    keyX++;
+                    yield return new WaitForSeconds(0.005f);
+                }
+                int keyXRot = -90;
+                while (keyXRot < 0)
+                {
+                    _key.transform.localRotation = Quaternion.Euler(keyXRot, 270, 0);
+                    keyXRot++;
+                    yield return new WaitForSeconds(0.01f);
+                }
+
             int y = 0;
             while (y >= -90)
             {
@@ -36,5 +56,20 @@ public class DoorObject : InteractObject
         //    }
 
         //}
+        //int keyXRot = 90;
+        //while (keyXRot > 0)
+        //{
+        //    _key.transform.localRotation = Quaternion.Euler(keyXRot, 270, 0);
+        //    keyXRot--;
+        //    yield return new WaitForSeconds(0.01f);
+        //}
+        //int keyX = 40;
+        //while (keyX > 0)
+        //{
+        //    _key.transform.localPosition += new Vector3(0, 0, 0.00001f);
+        //    keyX--;
+        //    yield return new WaitForSeconds(0.01f);
+        //}
+        //_key.SetActive(false);
     }
 }
